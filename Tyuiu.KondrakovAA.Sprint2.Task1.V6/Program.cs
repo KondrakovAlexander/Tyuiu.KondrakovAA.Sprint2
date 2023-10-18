6d39c252b252c426b586a691e0d0095966534b45
KondrakovAlexander <144276548+KondrakovAlexander@users.noreply.github.com>
2023-10-18 22:31:40 +0500

    Task1

---
+++ Tyuiu.KondrakovAA.Sprint2.Task1.V6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KondrakovAA.Sprint2.Task1.V6.Lib;
+
+namespace Tyuiu.KondrakovAA.Sprint2.Task1.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            DataService resp = new DataService();
+
+            a = 915;
+            b = 169;
+            c = 174;
+            d = 133;
+
+            bool[] res = resp.GetLogicOperations(a, b, c, d);
+
+            Console.Title = "Спринт #2 | Выполнил: Кондраков А.А. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************"); 
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Кондраков Александр Артемович | СМАРТб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ: Написать программу из операций сравнений (==, !=, <, >, <=, >=,*");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз           *");
+            Console.WriteLine("* в выражении) и логических операций (|, &, ||, &&, !, ^,                 *");
+            Console.WriteLine("* последовательность операций не должна нарушаться), а также              *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую последовательность  *");
+            Console.WriteLine("* (массив): (False, False, True, False, True, False),                     *");
+            Console.WriteLine("*  при a = 915, b = 169, c = 174, d = 133                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine($"c = {c}");
+            Console.WriteLine($"d = {d}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
